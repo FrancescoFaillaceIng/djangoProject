785a2316eee39b8cc28fd41a114c51639f35fe10
Francesco <francesco.faillace@stud.unifi.it>
2023-06-14 13:33:21 +0200

    railway

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-$x-$$!f$1pk4!p8f$nv=wrm+4pibu3au3&eom6+y7q^njgky(x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000']
 
 
 # Application definition
@@ -131,5 +132,4 @@ LOGIN_REDIRECT_URL = 'recipes-home'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = '/media/'
-
 
